93d309b0088f6a209ef5865297a2fed829bfbad5
andrewcell <andrew@iryu.kr>
2016-08-21 23:03:52 +0900

    Move Jessica function to Internal

--- CommClient.CSharp.Library/HTTP.cs
+++ CommClient.CSharp.Library/HTTP.cs
@@ -4,11 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.IO;
 
 namespace CommClient.CSharp.Library
 {
-    class HTTP
+    public class HTTP
     {
+      
 
+        private static HttpWebRequest req;
+        public static CookieContainer cookie = new CookieContainer();
+        private static Uri url;
+        private static string ServerURL;
+        public bool Login(string username, string password)
+        {
+            try
+            {
+                req = (HttpWebRequest)WebRequest.Create(url);
+                req.UserAgent = Information.UA;
+                req.Method = "POST";
+                req.ContentType = "text/plain";
+                req.CookieContainer = cookie;
+                Stream stream = req.GetRequestStream();
+                ASCIIEncoding encoding = new ASCIIEncoding();
+                byte[] byte1 = encoding.GetBytes(username);
+                stream.Write(byte1, 0, byte1.Length);
+                HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string tmp = reader.ReadToEnd();
+                var a = XML.ParseXML(username, password);
+                return true;
+            }
+            catch (Exception e)
+            {
+                //return "error - " + e.Message;
+                return false;
+            }
+
+        }
+        public bool setURL(string URL)
+        {
+            if (URL != "")
+            {
+                return false;
+            }
+            else
+            {
+                url = new Uri(URL);
+                ServerURL = URL;
+                return true;
+            }
+        }
     }
 }
