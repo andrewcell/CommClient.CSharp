6d7464513f3db6a33f7061c3b169db83d4ea985b
andrewcell <andrew@iryu.kr>
2016-08-22 21:58:27 +0900

    Add more Encrypt

--- CommClient.CSharp.Library/Encryption.cs
+++ CommClient.CSharp.Library/Encryption.cs
@@ -7,6 +7,40 @@ using System.Security.Cryptography;
 
 namespace CommClient.CSharp.Library
 {
+    public class Encryption
+    {
+        public string EncryptData(string data)
+        {
+            Information infor = new Information();
+            if(infor.Encrypttype == 0)
+            {
+                return sha256.Encrypt(data);
+
+            }
+            else if (infor.Encrypttype == 1)
+            {
+                return sha1.Encrypt(data);
+            }
+            else if(infor.Encrypttype == 2)
+            {
+                return sha512.Encrypt(data);
+            }
+            else if(infor.Encrypttype == 3)
+            {
+                return sha3.Encrypt(data);
+            }
+            else if(infor.Encrypttype == 4)
+            {
+                return md5.Encrypt(data);
+            }
+            else
+            {
+                return "Unknown Error";
+            }
+        }
+
+        //public string encrypt(string data, SHA256 asd)
+    }
     class md5
     {
         public static string Encrypt(string Raw)
@@ -38,4 +72,48 @@ namespace CommClient.CSharp.Library
         }
 
     }
+    public class sha1
+    {
+        public static string Encrypt(string raw)
+        {
+            SHA1 sha = new SHA1Managed();
+            byte[] byt = sha.ComputeHash(Encoding.ASCII.GetBytes(raw));
+            StringBuilder sb = new StringBuilder();
+            foreach (byte by in byt)
+            {
+                sb.AppendFormat("{0:x2}", by);
+            }
+            return sb.ToString();
+        }
+    }
+    public class sha512
+    {
+        public static string Encrypt(string raw)
+        {
+            SHA512 sha = new SHA512Managed();
+            byte[] byt = sha.ComputeHash(Encoding.ASCII.GetBytes(raw));
+            StringBuilder sb = new StringBuilder();
+            foreach (byte by in byt)
+            {
+                sb.AppendFormat("{0:x2}", by);
+            }
+            return sb.ToString();
+        }
+    }
+    public class sha3
+    {
+        public static string Encrypt(string raw)
+        {
+            SHA384 sha = new SHA384Managed();
+            byte[] byt = sha.ComputeHash(Encoding.ASCII.GetBytes(raw));
+            StringBuilder sb = new StringBuilder();
+            foreach (byte by in byt)
+            {
+                sb.AppendFormat("{0:x2}", by);
+            }
+            return sb.ToString();
+        }
+    }
+
+    
 }
